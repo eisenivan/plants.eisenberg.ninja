17264732198b05d7b14358c3f939b26d7ad39f3f
Ivan Eisenberg <ivaneisenberg@northwesternmutual.com>
2017-08-29 15:12:42 -0500

    updated config with google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,7 +38,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-105605485-1`,
       },
     },
     `gatsby-plugin-offline`,
---
+++ src/pages/2017-08-29-lemon-balm/index.md
@@ -0,0 +1,8 @@
+---
+title: Lemon Balm
+date: "2017-08-29"
+path: "/lemon-balm/"
+plants: true
+---
+
+The lemon balm against our back fence has a story behind it.
